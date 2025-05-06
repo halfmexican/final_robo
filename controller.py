6fe22d3ffc9655c00bddf0c8422b5c5f57c33715
half_mexican <halfmexicanhalfamazing@gmail.com>
2025-05-06 16:04:50 -0500

    init commit

---
+++ controller.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env pybricks-micropython
+
+# Regina Controller EV3
+# Copyright (C) 2025 Jose H
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pybricks.hubs import EV3Brick
+from pybricks.tools import wait, StopWatch
+from pybricks.parameters import Button
+from pybricks.messaging import BluetoothMailboxClient, TextMailbox
+
+# Commands
+COMMAND_DELAY_MS = 150 
+STOP_COMMAND_DRIVE = "STOP_DRIVE"
+STOP_COMMAND_ARM = "STOP_ARM"
+STOP_COMMAND_ALL = "STOP_ALL" 
+FORWARD_COMMAND = "FORWARD"
+BACKWARD_COMMAND = "BACKWARD"
+LEFT_COMMAND = "LEFT"
+RIGHT_COMMAND = "RIGHT"
+ARM_UP_COMMAND = "ARM_UP"
+ARM_DOWN_COMMAND = "ARM_DOWN"
+
+ev3 = EV3Brick()
+ev3.speaker.set_speech_options(language='es-la', voice='f1') # 🇲🇽
+
+# Bluetooth
+client = BluetoothMailboxClient()
+mbox = TextMailbox('control', client)
+
+print("Controller Ready.")
+print("Connecting to Ricardo")
+ev3.speaker.beep()
+
+try:
+    client.connect("Ricardo")
+    print("Connected!")
+    wait(5000)
+    ev3.speaker.say("Connectado! Hola Ricardo")
+    ev3.speaker.beep(frequency=100, duration=200)
+except Exception as e:
+    print("Connection failed:", e)
+    wait(5000)
+    exit()
+
+# Main Control Loop
+last_command_sent = None 
+command_timer = StopWatch() # Timer to manage command sending rate
+
+while True:
+    # Get the list of pressed buttons
+    pressed = ev3.buttons.pressed()
+    current_command = None
+
+    # need this check lol
+    if pressed is None:
+        wait(50) 
+        continue 
+
+    # Check for modifier key first
+    if Button.CENTER in pressed:
+        # Center is held down - Arm control mode
+        if Button.UP in pressed:
+            current_command = ARM_UP_COMMAND
+        elif Button.DOWN in pressed:
+            current_command = ARM_DOWN_COMMAND
+        else:
+            current_command = STOP_COMMAND_ARM
+    else:
+        # Center is NOT held down - Driving mode
+        if Button.UP in pressed:
+            current_command = FORWARD_COMMAND
+        elif Button.DOWN in pressed:
+            current_command = BACKWARD_COMMAND
+        elif Button.LEFT in pressed:
+            current_command = LEFT_COMMAND
+        elif Button.RIGHT in pressed:
+            current_command = RIGHT_COMMAND
+        else:
+            # No drive buttons pressed while Center is not pressed
+            # Determine what needs stopping based on the last command
+            if last_command_sent in [FORWARD_COMMAND, BACKWARD_COMMAND, LEFT_COMMAND, RIGHT_COMMAND]:
+                 current_command = STOP_COMMAND_DRIVE
+            elif last_command_sent in [ARM_UP_COMMAND, ARM_DOWN_COMMAND]: # Check if arm was moving
+                 current_command = STOP_COMMAND_ARM
+
+    # Send if the command changed OR if it's a continuous movement command
+    # And enough time has passed since the last command
+    is_movement_command = current_command not in [None, STOP_COMMAND_DRIVE, STOP_COMMAND_ARM, STOP_COMMAND_ALL]
+
+    if current_command != last_command_sent:
+        # If command changed, send it immediately
+        print("Sending:", current_command)
+        # Send STOP_ALL only if current_command becomes explicitly None after movement
+        # If current_command is one of the specific STOP commands, send that.
+        command_to_send = current_command
+        if command_to_send is None:
+             # Decide if doing nothing should send a general stop or just nothing
+             # Sending nothing might be better unless you want explicit stop on button release
+             # Let's refine: only send STOP if it was explicitly set above
+             pass # Don't send anything if command is None
+        else:
+            mbox.send(command_to_send)
+            last_command_sent = current_command # Update last_command_sent only when sending
+            command_timer.reset()
+    elif is_movement_command and command_timer.time() > COMMAND_DELAY_MS:
+        # If it's the same movement command, resend periodically
+        mbox.send(current_command)
+        command_timer.reset()
+
+    # Small delay to prevent busy-waiting
+    wait(20)
---
+++ main.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env pybricks-micropython
+
+# Ricardo Controller EV3
+# Copyright (C) 2025 Jose H
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port, Stop, Direction
+from pybricks.tools import wait
+from pybricks.messaging import BluetoothMailboxServer, TextMailbox
+
+# Configuration
+DRIVE_SPEED = 400  # degrees/second
+TURN_RATE = 150    # degrees/second
+ARM_SPEED = 100    # degrees/second
+
+# Commands
+STOP_COMMAND_DRIVE = "STOP_DRIVE"
+STOP_COMMAND_ARM = "STOP_ARM"
+STOP_COMMAND_ALL = "STOP_ALL"
+FORWARD_COMMAND = "FORWARD"
+BACKWARD_COMMAND = "BACKWARD"
+LEFT_COMMAND = "LEFT"
+RIGHT_COMMAND = "RIGHT"
+ARM_UP_COMMAND = "ARM_UP"
+ARM_DOWN_COMMAND = "ARM_DOWN"
+
+
+# Objects 
+ev3 = EV3Brick()
+ev3.speaker.set_speech_options(language='es-la', voice='m1') # 🇲🇽
+
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+arm_motor = Motor(Port.D) 
+
+# Bluetooth
+server = BluetoothMailboxServer()
+mbox = TextMailbox('control', server)
+print("Robot Waiting for Connection...")
+ev3.speaker.beep()
+server.wait_for_connection()
+print("Connected!")
+ev3.speaker.beep(frequency=500, duration=200)
+ev3.speaker.say("Connectado! Hola Regina")
+
+# Main
+while True:
+    received_command = mbox.read()
+
+    if received_command:
+        print("Received:", received_command)
+
+        # Motor Control
+        if received_command == FORWARD_COMMAND:
+            left_motor.run(DRIVE_SPEED)
+            right_motor.run(DRIVE_SPEED)
+        elif received_command == BACKWARD_COMMAND:
+            left_motor.run(-DRIVE_SPEED)
+            right_motor.run(-DRIVE_SPEED)
+        elif received_command == LEFT_COMMAND:
+            left_motor.run(-TURN_RATE)
+            right_motor.run(TURN_RATE)
+        elif received_command == RIGHT_COMMAND:
+            left_motor.run(TURN_RATE)
+            right_motor.run(-TURN_RATE)
+        # Add a STOP command handling
+        elif received_command == STOP_COMMAND_DRIVE:
+             left_motor.stop()
+             right_motor.stop()
+        # Arm Motor Control
+        elif received_command == ARM_UP_COMMAND:
+            arm_motor.run(ARM_SPEED) 
+        elif received_command == ARM_DOWN_COMMAND: 
+            arm_motor.run(-ARM_SPEED) 
+        elif received_command == STOP_COMMAND_ARM:
+             arm_motor.stop()
+
+        #Stop All
+        elif received_command == STOP_COMMAND_ALL: 
+             left_motor.stop()
+             right_motor.stop()
+             arm_motor.stop()
+    else:
+
+        pass
+
+    # Small delay
+    wait(10)
